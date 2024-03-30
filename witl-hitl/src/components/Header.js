429ca73a2715a6fbebe646b2986152f985ead418
ankit-11525 <ankitpathak11525@gmail.com>
2024-03-31 02:53:26 +0530

    login logout functioning done

--- witl-hitl/src/components/Header.js
+++ witl-hitl/src/components/Header.js
@@ -1,6 +1,9 @@
+import { getServerSession } from "next-auth";
 import Link from "next/link";
-
-export default function Header() {
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import LogoutButton from "./buttons/LogoutButton";
+export default async function Header() {
+  const session = await getServerSession(authOptions);
   return (
     <header className="bg-white border-b py-6 ">
       <div className="max-w-4xl mx-auto flex justify-between px-6 ">
@@ -13,8 +16,25 @@ export default function Header() {
           </nav>
         </div>
         <div className="flex items-center gap-4 text-slate-500 text-sm">
-          <Link href={"/login"}>Sign In</Link>
-          <Link href={"/register"}> Create Account</Link>
+          
+        {
+            !!session && (<>
+            <Link href={'/account'}>
+            <div> Hello!  {session?.user?.name}</div>
+            </Link>
+            <LogoutButton />
+            
+            </>)
+          }
+          
+          
+          {!session && (
+            <>
+              <Link href={"/login"}>Sign In</Link>
+              <Link href={"/register"}> Create Account</Link>
+            </>
+          )}
+          
         </div>
       </div>
     </header>
---
+++ witl-hitl/src/components/buttons/LogoutButton.js
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {signOut} from "next-auth/react";
+
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+const LogoutButton = ({
+    className = 'flex items-center gap-2 border rounded-lg p-2 px-4 shadow ',
+    iconLeft = false,
+    iconClasses = '',
+  }) => {
+  return (
+    <button className={className} onClick={() => signOut()}>
+      {iconLeft && (
+        <FontAwesomeIcon icon={faRightFromBracket} className={iconClasses} />
+      )}
+      <span>Logout</span>
+      {!iconLeft && (
+        <FontAwesomeIcon icon={faRightFromBracket} className={iconClasses} />
+      )}
+    </button>
+  );
+};
+
+export default LogoutButton;
